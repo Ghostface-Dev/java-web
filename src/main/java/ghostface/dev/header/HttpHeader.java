5053b4e3882c2c1a7c9a149e4043cb7d4c28c38a
ghost_face_dev <henriquedev6@gmail.com>
2024-09-23 01:29:26 -0300

    defined header

--- src/main/java/ghostface/dev/header/HttpHeader.java
+++ src/main/java/ghostface/dev/header/HttpHeader.java
@@ -1,4 +1,35 @@
 package ghostface.dev.header;
 
-public class HttpHeader {
+
+import ghostface.dev.header.content.ContentHeader;
+import ghostface.dev.header.general.GeneralHeader;
+import org.jetbrains.annotations.NotNull;
+
+public abstract class HttpHeader {
+
+    private final @NotNull GeneralHeader general;
+    private final @NotNull ContentHeader content;
+    private final @NotNull MediaType<?> mediaType;
+
+    protected HttpHeader(
+            @NotNull GeneralHeader general,
+            @NotNull ContentHeader content,
+            @NotNull MediaType<?> mediaType)
+    {
+        this.general = general;
+        this.content = content;
+        this.mediaType = mediaType;
+    }
+
+    public @NotNull GeneralHeader getGeneral() {
+        return general;
+    }
+
+    public @NotNull ContentHeader getContent() {
+        return content;
+    }
+
+    public @NotNull MediaType<?> getMediaType() {
+        return mediaType;
+    }
 }
