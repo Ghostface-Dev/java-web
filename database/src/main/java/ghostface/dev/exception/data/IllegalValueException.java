2395f94bd559f7baa782048fc02bf7a24d75dd9e
ghost_face_dev <henriquedev6@gmail.com>
2024-10-31 19:04:23 -0300

    making java-web independent

--- database/src/main/java/ghostface/dev/exception/data/IllegalValueException.java
+++
@@ -1,7 +0,0 @@
-package ghostface.dev.exception.data;
-
-public class IllegalValueException extends DataException {
-    public IllegalValueException(String message) {
-        super(message);
-    }
-}
--- database/src/main/java/ghostface/dev/operation/Crud.java
+++
@@ -1,9 +0,0 @@
-package ghostface.dev.operation;
-
-import ghostface.dev.mapping.Key;
-
-public interface Crud<T extends Key<?>>
-        extends Insertable<T>, Selectable<T>, Upgradable<T>, Deletable<T>
-{
-    // Do nothing
-}
